c126afa514c886fd7c9806ec725452553787499e
Saikiran chittimilla <saikiran.chittimilla@stud.h-da.de>
2020-05-21 21:58:24 +0200

    First Prototype of My WebPage

---
+++ js/myjs.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+console.log("jquery is working");
+$("#h-da").click(function(){
+	$('#MscPopup').addClass("active");
+	$('#MscOverlay').addClass('show');
+	$('body').css('overflow','hidden');
+	});
+$('#MscPopupClose').click(function(){
+	$('#MscPopup').removeClass("active");
+	$('#MscOverlay').removeClass('show');
+	$('body').css('overflow','auto');
+});
+$("#OU").click(function(){
+	$('#BEPopup').addClass("active");
+	$('#BEOverlay').addClass('show');
+	$('body').css('overflow','hidden');
+	});
+$('#BEPopupClose').click(function(){
+	$('#BEPopup').removeClass("active");
+	$('#BEOverlay').removeClass('show');
+	$('body').css('overflow','auto');
+});
+$("#Narayana").click(function(){
+	$('#InterPopup').addClass("active");
+	$('#InterOverlay').addClass('show');
+	$('body').css('overflow','hidden');
+	});
+$('#InterPopupClose').click(function(){
+	$('#InterPopup').removeClass("active");
+	$('#InterOverlay').removeClass('show');
+	$('body').css('overflow','auto');
+});
+});
